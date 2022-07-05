b2b938ce3e7f96bb0cef40c9546b91a50292f32f
Sadik <sadiqulislam711@gmail.com>
2022-07-05 14:14:47 +0530

    commit8

---
+++ frontend/src/Component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div>
+      <div className="wrapper">
+      <div><p>Logo</p></div>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Projects</a></li>
+        <li><a href="#">Content</a></li>
+      </ul>
+    </div>
+    </div>
+  )
+}
