cc8aecfd89567f106cd4d4225009518610efdf60
Sadik <sadiqulislam711@gmail.com>
2022-07-05 14:34:17 +0530

    commit10

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import FormComponent from './Component/FormComponent';
+import Header from './Component/Header';
 
 function App() {
   return (
     <>
+    <Header/>
    <FormComponent/>
     </>
   );
--- frontend/src/Component/Header.js
+++ frontend/src/Component/Header.js
@@ -1,16 +1,20 @@
 import React from 'react';
-
+import { Navbar,Nav,Container } from 'react-bootstrap';
 export default function Header() {
   return (
-    <div>
-      <div className="wrapper">
-      <div><p>Logo</p></div>
-      <ul>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Content</a></li>
-      </ul>
-    </div>
-    </div>
+      <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <br />
+  
+    </>
   )
 }
