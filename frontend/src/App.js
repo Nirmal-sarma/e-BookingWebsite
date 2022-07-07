fc3a442343c536959cbe8c4649fc78d1b65d6230
VikasPc <Your Email Address Here>
2022-07-07 15:27:23 +0530

    cmmit8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,29 +11,23 @@ import Bus from './Component/pages/Bus';
 import Hotel from './Component/pages/Hotel';
 import Home from './Component/pages/Home';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-     <Router>
-      <Header />
-      <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route exact path="/Flight" element={<Flight/>}/>
-      <Route exact path="/Train" element={<Train/>}/>
-      <Route exact path="/Bus" element={<Bus/>}/>
-      <Route exact path="/Hotel" element={<Hotel/>}/>
-      
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/Flight" element={<Flight />} />
+          <Route exact path="/Train" element={<Train />} />
+          <Route exact path="/Bus" element={<Bus />} />
+          <Route exact path="/Hotel" element={<Hotel />} />
         </Routes>
-      <Cards/>
-       <Slider />
-      </Router> 
+        <Cards />
+        <Slider />
+      </Router>
     </>
   );
 }
