dfb6e0f74b6ccc83d41503bce0d8317f783df8db
VikasPc <Your Email Address Here>
2022-07-08 11:01:31 +0530

    rmoveUnlnInNav

--- frontend/src/Component/Header.js
+++ frontend/src/Component/Header.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import './Header.css';
-import { Navbar,Nav,Container } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 export default function Header() {
   return (
-      <>
-      <Navbar className="NavBar"  variant="light">
+    <>
+      <Navbar className="NavBar" variant="light">
         <Container className="NavBarContainer">
-        <Link className="Logo" to="/">LetsGo</Link>
-        <Nav className="me-auto">
-        
-          <Link className="Page"  to="/Flight"><div class="Page">Flight</div></Link>
-          <Link className="Page" to="/Train"><div class="Page">Train</div></Link>
-          <Link className="Page" to="/Bus"><div class="Page">Bus</div></Link>
-          <Link className="Page" to="/Hotel"><div class="Page">Hotels</div></Link>
-
-        </Nav>
-        <Nav.Link className='signIn' to="#Signin">SignIn</Nav.Link>
+          <Link style={{ textDecoration: 'none' }} className="Logo" to="/">
+            LetsGo
+          </Link>
+          <Nav className="me-auto">
+            <Link
+              style={{ textDecoration: 'none' }}
+              className="Page"
+              to="/Flight"
+            >
+              <div class="Page">Flight</div>
+            </Link>
+            <Link
+              style={{ textDecoration: 'none' }}
+              className="Page"
+              to="/Train"
+            >
+              <div class="Page">Train</div>
+            </Link>
+            <Link style={{ textDecoration: 'none' }} className="Page" to="/Bus">
+              <div class="Page">Bus</div>
+            </Link>
+            <Link
+              style={{ textDecoration: 'none' }}
+              className="Page"
+              to="/Hotel"
+            >
+              <div class="Page">Hotels</div>
+            </Link>
+          </Nav>
+          <Nav.Link className="signIn" to="#Signin">
+            SignIn
+          </Nav.Link>
         </Container>
       </Navbar>
       <br />
-  
     </>
-  )
+  );
 }
